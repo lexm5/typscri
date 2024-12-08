0ecf2f96c9e046bc097842a5acd1ed7c0a56b027
DESKTOP-O7DKLSP\miyake <20011341mk@gmail.com>
2024-12-08 21:44:01 +0900

    deleteボタンの追加

--- device-management/frontend/src/components/DeviceList/DeviceList.tsx
+++ device-management/frontend/src/components/DeviceList/DeviceList.tsx
@@ -13,15 +13,27 @@ const DeviceList: React.FC = () => {
     const [devices, setDevices] = useState<Device[]>([]); //<Device[]>([])はTypescriptのアノテーション, []は状態の初期値
 
     useEffect(() => {
+        fetchDevices();
+    }, []);
+
+    const fetchDevices = () => {
         axios.get('http://localhost:5000/api/devices')
             .then((response) => {
                 setDevices(response.data);
-            })
-            .catch((error) => {
+            }).catch((error) => {
                 console.error('Error fetching devices:', error);
             });
-    }, []);
+    }
 
+    const handleDelete = (id: number) => {
+        axios.delete(`http://localhost:5000/api/devices/${id}`)
+            .then(() => {
+                //削除後に再度一覧を取得
+                fetchDevices();
+            }).catch((error) => {
+                console.error('Error deleteing device:', error);
+            })
+    }
     return (
         <div className={styles.container}>
             <div className={styles.header}>Device List</div>
@@ -29,6 +41,8 @@ const DeviceList: React.FC = () => {
                 <div key={device.id} className={styles.deviceItem}>
                     <span>{device.name} ({device.type})</span>
                     <span>{new Date(device.created_at).toLocaleDateString()}</span>
+                    {/*button処理の追加 */}
+                    <button onClick={() => handleDelete(device.id)} className={styles.deleteButton}>Delete</button>
                 </div>
             ))}
         </div>
